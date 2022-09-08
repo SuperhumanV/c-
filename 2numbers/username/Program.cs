ca979452d563038945cfe73f21d5a0e6a8567674
Vad <poriata@yandex.ru>
2022-09-08 19:21:28 +0300

    special username

---
+++ 2numbers/username/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Username");
+string Username = Console.ReadLine();
+
+if (Username.ToLower() =="zhongli")
+{
+Console.WriteLine("Osmanthus wine tastes the same as i remember");
+}
+else
+{
+Console.WriteLine("Welcome to Mondstat");
+}
