253b3535541badf9510a2ad79e9e033d74778d2c
Vad <poriata@yandex.ru>
2022-09-08 19:17:24 +0300

    summ of 2

---
+++ 2numbers/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число");
+int a = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Суммa");
+Console.WriteLine(a+b);
